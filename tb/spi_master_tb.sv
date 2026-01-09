67a647003c6381a08f850b8e4776a45518c85663
vp278 <vp278@cornell.edu>
2026-01-09 16:24:47 -0500

    check master->slave after cpu->master

--- tb/spi_master_tb.sv
+++ tb/spi_master_tb.sv
@@ -1,24 +1,29 @@
+// the testbench acts as both slave and host/CPU
+// as slave, it reacts to miso and reports mosi
+// as CPU, it sends data_in and 
+
 class spi_transaction #(int DATA_LENGTH = 8);
-	rand bit [DATA_LENGTH-1:0] tx_data; // data from outside world to send to slave
-	rand bit [DATA_LENGTH-1:0] rx_data; // data from slave to outside world
+	rand bit [DATA_LENGTH-1:0] tx_data; // data from CPU to send to slave
+	rand bit [DATA_LENGTH-1:0] rx_data; // data from slave to CPU
 
 	constraint tx_no_zero {tx_data != 8'h00;}
 	constraint rx_no_zero {rx_data != 8'h00;}
 endclass
 
-module spi_slave_tb;
+module spi_master_tb;
     parameter DATA_LENGTH = 8;
     parameter CLK_DIV = 4;
     logic clk;
     logic rst_n;
     logic [DATA_LENGTH-1:0] data_in;  // data from CPU
-    logic [DATA_LENGTH-1:0] data_out; // data to CPUS
+    logic [DATA_LENGTH-1:0] data_out; // data to CPU
     logic start;
     logic busy;
     logic done;
     spi_bus_if spiIF(); // instantiating the bus interface
 
-    bit [7:0] data_out_tb;
+    bit [DATA_LENGTH-1:0] data_out_tb;
+	bit [DATA_LENGTH-1:0] data_from_master;
     spi_transaction #(8) spi_obj;
 
     spi_master #(.DATA_LENGTH(DATA_LENGTH), .CLK_DIV(CLK_DIV)) s1( .clk(clk), 
@@ -57,11 +62,10 @@ module spi_slave_tb;
     	for (int t = 0; t < 5; t++) begin
         	spi_obj = new();
         	assert(spi_obj.randomize());
+			//data_from_master = '0;
 
         	data_in = spi_obj.tx_data;
         	data_out_tb = spi_obj.rx_data;
-
-        	$display("Transaction %0d: Sending to slave: %0h, Slave sends: %0h", t+1, data_in, data_out_tb);
 
         	// Generate single-cycle start pulse
         	@(posedge clk);
@@ -73,7 +77,13 @@ module spi_slave_tb;
         	for (int i = DATA_LENGTH-1; i >= 0; i--) begin
             	@(posedge spiIF.spi_sck);
             	spiIF.spi_miso = data_out_tb[i];
+				@(negedge spiIF.spi_sck);
+				data_from_master[i] = spiIF.spi_mosi;
         	end
+
+			$display("\n\nTransaction %0d", t+1);
+        	$display("CPU- CPU to master: %0h", data_in);
+        	$display("SLAVE- master to slave: %0h, slave to master: %0h", data_from_master, data_out_tb);
 
         	// Wait for master to finish transaction
         	wait(done);
---
+++ workdir/xcelium.d/run.lnx8664.24.03.d/files.ts
@@ -0,0 +1,3 @@
+1767981399 /home/vp278/Documents/spi_master/uvm/spi_bus_if.sv
+1767992707 /home/vp278/Documents/spi_master/rtl/spi_master.sv
+1767993730 /home/vp278/Documents/spi_master/tb/spi_master_tb.sv
