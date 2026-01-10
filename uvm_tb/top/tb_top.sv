a91a9bed45deccba71c705f78a19a4a03053e369
vp278 <vp278@cornell.edu>
2026-01-10 16:57:58 -0500

    add random packet as host to master

--- uvm_tb/top/tb_top.sv
+++ uvm_tb/top/tb_top.sv
@@ -1,3 +1,8 @@
+class spi_host_transaction #(int DATA_LENGTH);
+	rand bit [DATA_LENGTH-1:0] data_host_to_master;
+endclass
+
+
 module tb_top;
 	import uvm_pkg::*;
 	import spi_pkg::*;
@@ -8,17 +13,23 @@ module tb_top;
 	logic clk, rst_n;
 	bit start, busy, done;
 
+	bit [DATA_LENGTH-1:0] data_in;
+	bit [DATA_LENGTH-1:0] data_out;
+
 	// Instantiate DUT
 	spi_master #(.DATA_LENGTH(DATA_LENGTH)) dut (
 		.clk(clk),
 		.rst_n(rst_n),
-		.data_in(8'hAA), // for now, fixed value
-		.data_out(), // only check master to slave for now
+		.data_in(data_in), 
+		.data_out(data_out),
 		.start(start),
 		.busy(busy),
 		.done(done),
 		.spiIF(spi_if)
 		);
+
+	// create an object of spi_host_transaction class
+	spi_host_transaction #(DATA_LENGTH) txn;
 
 	// Handle external clock, reset and start chip select by pulling it down
 	initial clk = 0;
@@ -32,12 +43,22 @@ module tb_top;
 		#10
 		rst_n = 1;
 		start = 1; // trigger posedge rst_n
+		txn = new();
+	end
+
+	always_ff @(negedge spi_if.spi_sck) begin
+		if(start) begin
+			assert(txn.randomize());
+			data_in = txn.data_host_to_master;
+		end
 	end
 
 	// Provide interface handle to UVM
 	initial begin
 		uvm_config_db#(virtual spi_bus_if)::set(null, "*", "vif", spi_if);;
 		run_test("spi_test");
+		if (done)
+			$display("TOP- host to master: %0h, master to host: %h", data_in, data_out);
 	end
 
 endmodule
