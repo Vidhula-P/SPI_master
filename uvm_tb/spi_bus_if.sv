d5b1c13421a10a94622d8f3683aeb807dd648e8f
vp278 <vp278@cornell.edu>
2026-01-22 11:38:23 -0500

    Add clocking blocks

--- uvm_tb/spi_bus_if.sv
+++ uvm_tb/spi_bus_if.sv
@@ -5,4 +5,10 @@ interface spi_bus_if;
     logic spi_cs_n;
     logic spi_mosi;
     logic spi_miso;
+
+    // clocking blocks for the TB
+	clocking spi_cb @(posedge spi_sck); //sample on rising edge
+        output spi_cs_n, spi_mosi;
+        input spi_miso;
+    endclocking
 endinterface
--- uvm_tb/spi_host_if.sv
+++ uvm_tb/spi_host_if.sv
@@ -5,4 +5,10 @@ interface spi_host_if #(int DATA_LENGTH = 8) (input logic clk);
 	logic done;
 	logic [DATA_LENGTH-1:0] host_out;
 	logic [DATA_LENGTH-1:0] host_in;
+
+	// clocking blocks for the TB
+	clocking host_cb @(posedge clk);
+        output rst_n, start, host_out;
+        input busy, done, host_in;
+    endclocking
 endinterface
